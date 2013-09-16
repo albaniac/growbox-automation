396b5d5571c46c07283d2823e8a0e69ef6fea582
Max Lunin <nut.code.monkey@gmail.com>
2013-09-16 16:00:30 +0300

    refactoring

--- GrowBox/GrowBox.ino
+++ GrowBox/GrowBox.ino
@@ -9,47 +9,70 @@
  *
  * Required DallasTemperature_360 library
  */
-
-
+ 
 #include <OneWire.h>
 #include <DallasTemperature.h>
 
 // Data wire is plugged into port 2 on the Arduino
-#define ONE_WIRE_BUS 2
-#define FAN 13
+#define TEMPERATURE_SENSOR_PIN 2
+#define FAN_PIN 13
 
-// Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
-OneWire oneWire(ONE_WIRE_BUS);
+// Setup a oneWire instance to communicate with any OneWire devices 
+// (not just Maxim/Dallas growBoxTemperature ICs)
+OneWire oneWire(TEMPERATURE_SENSOR_PIN);
 
 // Pass our oneWire reference to Dallas Temperature.
 DallasTemperature sensors(&oneWire);
 
-void setup(void)
+#define ON LOW
+#define OFF HIGH
+
+void turnFans(int flag)
 {
-    pinMode(FAN, OUTPUT);   
-    digitalWrite(FAN, LOW);
-  
-    // start serial port
-    Serial.begin(9600);
-    Serial.println("Dallas Temperature IC Control Library Demo");
-    
-    // Start up the library
-    sensors.begin();
+  digitalWrite(FAN_PIN, flag);
 }
 
-void loop(void)
+void setup()
 {
-    // call sensors.requestTemperatures() to issue a global temperature
-    // request to all devices on the bus
-    Serial.print("Requesting temperatures...");
-    sensors.requestTemperatures(); // Send the command to get temperatures
-    Serial.println("DONE");
-    
-    Serial.print("Temperature for the device 1 (index 0) is: ");
-    
-    float temperature = sensors.getTempCByIndex(0);
-    
-    Serial.println(temperature);
-    
-    digitalWrite(FAN, (temperature > 30) ? HIGH : LOW);
+  pinMode(FAN_PIN, OUTPUT);   
+  turnFans(OFF);
+  
+  // начинаем передавать 
+  Serial.begin(9600);
+ 
+  // Начинаем получать данные от датчика
+  sensors.begin();
+}
+
+/*
+ * Диапазон температур от -55°C до +125°C
+ * Погрешность преобразования меньше 0,5°C в диапазоне контролируемых температур -10°C до +85°С
+ * Источник http://arduino-ua.com/prod190-Datchik_temperatyri_DS18B20
+ *
+ * @return возвращает температуру в градусах цельсия
+ */
+float growBoxTemperature()
+{ // можно подключить несколько сенсоров на оди вход
+  // вызов sensors.requestTemperatures() опрашивает все сенсоры
+  sensors.requestTemperatures(); 
+  
+  // запрашиваем обновленную информацию по первому сенсору
+  float temperature = sensors.getTempCByIndex(0);
+  
+  Serial.print("Temperature is: "); Serial.println(temperature);
+  return temperature;
+}
+
+const static float maxAllowedTemperature = 26.; // °C 
+
+void loop()
+{
+  if( growBoxTemperature() > maxAllowedTemperature )
+  {
+    turnFans(ON);
+  }
+  else
+  {
+    turnFans(OFF);
+  }
 }
