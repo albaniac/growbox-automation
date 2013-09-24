49fdb975661feb77ee93ec2d1b0bde70bfc0ef3e
Max Lunin <nut.code.monkey@gmail.com>
2013-09-24 14:40:00 +0300

    Refactoring

--- GrowBox/GrowBox.ino
+++ GrowBox/GrowBox.ino
@@ -14,7 +14,7 @@
   
 #include <OneWire.h>
 #include <DallasTemperature.h>
-/* Подключение Сенсор температуры:
+/* Подключение датчика температуры:
  *
  * Диапазон температур от -55°C до +125°C. Погрешность преобразования меньше 0,5°C в диапазоне контролируемых температур -10°C до +85°С
  * Источник http://arduino-ua.com/prod190-Datchik_temperatyri_DS18B20
@@ -34,58 +34,65 @@ static const uint8_t TEMPERATURE_SENSOR_PIN = 2;
  * IN1 - FAN_PIN (3) */
 static const uint8_t FAN_PIN = 3;
 
-class Fan {
-    uint8_t pin;
-    
-    public:
+typedef class Fan
+{
+    const uint8_t pin;
+
+public:
     
     static const uint8_t ON = HIGH;
     static const uint8_t OFF  = LOW;
 
-    void turn(uint8_t mode) {
-        if(digitalRead(pin) != mode) {
+    inline void turn(uint8_t mode) const
+    {
+        if(digitalRead(pin) != mode)
+        {
             digitalWrite(pin, mode);
         }
     }
 
-    Fan(uint8_t fanPin): pin(fanPin) {
+    Fan(uint8_t fanPin) : pin(fanPin)
+    {
         pinMode(pin, OUTPUT);
-        digitalWrite(pin, OFF);
+        digitalWrite(pin, OFF);// sic! 
         digitalWrite(pin, ON); // устанавливаем высокий уровень для инициализации реле
     };
     
-    bool isTurnedOff() {
+    inline bool isTurnedOff() const
+    {
         return digitalRead(pin) == OFF;
     }
 };
 
 
+#include <LCD5110_Graph.h>
 /* Модуль Nokia 5110 LCD
  * Источник http://arduino-ua.com/prod407-Nokia5110_LCD_modyl */
-static const uint8_t DISPLAY_RST  = 8;  // LCD RST .... Pin 1 - Reset
-static const uint8_t DISPLAY_CS   = 7;  // LCD CE  .... Pin 2 - выбор чипа
-static const uint8_t DISPLAY_DC   = 6;  // LCD DC ..... Pin 3 - переключатель данные/инструкции
-static const uint8_t DISPLAY_MOSI = 5;  // LCD Din .... Pin 4 - Serial Data Line
-static const uint8_t DISPLAY_SCK  = 4;  // LCD Clk .... Pin 5 - Serial Clock Line
+static const uint8_t LCD_RST_PIN  = 8;  // LCD RST .... Pin 1 - Reset
+static const uint8_t LCD_CS_PIN   = 7;  // LCD CE  .... Pin 2 - выбор чипа
+static const uint8_t LCD_DC_PIN   = 6;  // LCD DC ..... Pin 3 - переключатель данные/инструкции
+static const uint8_t LCD_MOSI_PIN = 5;  // LCD Din .... Pin 4 - Serial Data Line
+static const uint8_t LCD_SCK_PIN  = 4;  // LCD Clk .... Pin 5 - Serial Clock Line
                                         // LCD Vcc .... Pin 6 - (+3.3V) питание
                                         // LCD BL ..... Pin 7 - подсветка
-                                        // LCD Gnd .... Pin 8 - земля                  
-#include <LCD5110_Graph.h>
-
-struct GrowBox {
+                                        // LCD Gnd .... Pin 8 - земля
+class GrowBox
+{
+public:
     DallasTemperature* termoSensors;
+    Fan* fans;
+    LCD5110* display;
     
     float minTemperature;
     float maxTemperature;
-    
-    Fan* fans;
-    LCD5110* display;
    
+private:
     float getTemperature(bool& canFetch)
     {
         uint8_t countOfDevices = termoSensors->getDeviceCount();
         float temperature = 0;
-        if (countOfDevices > 0) {
+        if (countOfDevices > 0)
+        {
             termoSensors->requestTemperatures();
             // запрашиваем обновленную информацию по первому сенсору
             temperature = termoSensors->getTempCByIndex(0);
@@ -94,11 +101,22 @@ struct GrowBox {
         return temperature;
     }
 
-    void displayCurrentTemperature() {
+    void displayNoConnectedSensors()
+    {
+        display->clrScr();
+        display->print("No sensor", CENTER, 0);
+        display->print("connected", CENTER, 15);
+        display->update();
+    }
+
+public:
+    void displayCurrentTemperature()
+    {
         bool canFetchTemperature;
         float temperature = getTemperature(canFetchTemperature);
         
-        if (canFetchTemperature){
+        if (canFetchTemperature)
+        {
             display->clrScr();
             display->print("Temperature:", CENTER, 0);
             
@@ -106,53 +124,49 @@ struct GrowBox {
             display->print((char*)currentTemperature.c_str(), CENTER, 12);
             
             String separator = fans->isTurnedOff() ? String(" -> ") : String(" <- ");
-            String gape = String(minTemperature, 1) += separator += String(maxTemperature, 1);
-            display->print((char*)gape.c_str(), CENTER, 25);
-            
+            String interval = String(minTemperature, 1) += separator += String(maxTemperature, 1);
+            display->print((char*)interval.c_str(), CENTER, 25);
             
             String isTurned = String("Fan: ") += (fans->isTurnedOff() ? "off" : "on");
             display->print((char*)isTurned.c_str(), CENTER, 37);
             
             display->update();
         }
-        else {
+        else
+        {
             displayNoConnectedSensors();
             fans->turn(Fan::ON);
         }
     }
     
-    void updateFansState() {
+    void updateFansState()
+    {
         bool canFetchTemperature;
-        float temperature = getTemperature(canFetchTemperature);
-        if (canFetchTemperature) {
-             if (temperature < minTemperature /* && !fans->isTurnedOff() */ )
+        float temperature = getTemperature( canFetchTemperature );
+        if (canFetchTemperature)
+        {
+             if (temperature < minTemperature)
              {
-                 fans->turn(Fan::OFF);
-                 delay(100);
+                 fans->turn( Fan::OFF );
+                 delay(100); // 
              }
-             else if (temperature > maxTemperature && fans->isTurnedOff())
+             else if (temperature > maxTemperature)
              {
-                 fans->turn(Fan::ON);
+                 fans->turn( Fan::ON );
              }
         }
-        else {
+        else
+        {
             displayNoConnectedSensors();
-            if (fans->isTurnedOff()) 
-                fans->turn(Fan::ON);
+            fans->turn( Fan::ON );
         }
-    }
-    
-    void displayNoConnectedSensors() {
-        display->clrScr();
-        display->print("No sensor", CENTER, 0);
-        display->print("connected", CENTER, 15);
-        display->update();
     }
 };
 
 static GrowBox growBox;
 
-void setup() {
+void setup()
+{
 //    Serial.begin(9600);
     growBox.fans = new Fan(FAN_PIN);
     
@@ -165,14 +179,15 @@ void setup() {
     // Начинаем получать данные от датчика
     growBox.termoSensors->begin();
     
-    growBox.display = new LCD5110(DISPLAY_SCK, DISPLAY_MOSI, DISPLAY_DC, DISPLAY_RST, DISPLAY_CS);
+    growBox.display = new LCD5110(LCD_SCK_PIN, LCD_MOSI_PIN, LCD_DC_PIN, LCD_RST_PIN, LCD_CS_PIN);
     growBox.display->InitLCD();
 
     extern uint8_t SmallFont[]; 
     growBox.display->setFont(SmallFont);
 }
 
-void loop() {
+void loop()
+{
     growBox.displayCurrentTemperature();
     growBox.updateFansState();
 }
